cd2a74776d428be500fd54dcc88461ff091f2bc9
krista7298 <kriagu7298@students.ecpi.edu>
2026-05-21 10:37:19 -0400

    Refactor menu to use list for options
    Refactored menu display to use a list for options.

--- Project_Menu.py
+++ Project_Menu.py
@@ -1,12 +1,23 @@
 from datetime import datetime
 
 print("kriagu7298 Spreadsheet Automation Menu")
+
+menuOptions = [
+    "1. Input Data",
+    "2. View Current Data",
+    "3. Generate Report"
+]
+
 print("Choose a number from the following options")
-print("1. Input Data")
-print("2. View Current Data")
-print("3. Generate Report")
+
+# This loop variable represents each menu option in the list.
+for option in menuOptions:
+    print(option)
 
 # The next line retrieves the inputted option and stores into the variable called user_choice.
 user_choice = input("Enter your selection: ")
 
-print("You selected", user_choice, "at", str(datetime.now()))
+if user_choice == "1" or user_choice == "2" or user_choice == "3":
+    print("You selected", user_choice, "at", str(datetime.now()))
+else:
+    print("Error: Invalid choice selected")
