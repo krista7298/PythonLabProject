15ee69c27c707e146b854f48fa8812a1553e8099
krista7298 <kriagu7298@students.ecpi.edu>
2026-05-31 15:06:07 -0400

    Implement data conversion and input functions
    Added functions for data conversion and user input collection.

--- Project_Menu.py
+++ Project_Menu.py
@@ -14,10 +14,34 @@ print("Choose a number from the following options")
 for option in menuOptions:
     print(option)
 
+# Function to convert Fahrenheit to Celsius
+def convertData(tempF):
+    return (tempF - 32) * 5 / 9
+
+# Function to collect user input
+def getInput():
+
+    entryCount = int(input("How many entries are you inputting?\n"))
+
+    for counter in range(entryCount):
+
+        dateEntered = input("\nEnter a date:\n")
+
+        tempF = float(input("Enter the highest temp for the inputted date:\n"))
+
+        # Function: convertData
+        # Argument: temperature in Fahrenheit
+        # Returns: temperature in Celsius
+        convertedTemp = convertData(tempF)
+
+        print(f"The following was saved at {datetime.now()} :")
+        print(f"{dateEntered},{tempF},{convertedTemp}")
+
 # The next line retrieves the inputted option and stores into the variable called user_choice.
 user_choice = input("Enter your selection: ")
 
-if user_choice == "1" or user_choice == "2" or user_choice == "3":
+if user_choice == "1":
     print("You selected", user_choice, "at", str(datetime.now()))
+    getInput()
 else:
-    print("Error: Invalid choice selected")
+    print("Error: The chosen functionality is not implemented yet")
